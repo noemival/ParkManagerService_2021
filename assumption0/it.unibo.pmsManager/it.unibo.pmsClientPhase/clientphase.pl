c3fb00ca7fe9050cdc2345c10ea1ea4f94338744
noemival <49717556+noemival@users.noreply.github.com>
2021-07-12 19:12:57 +0200

    Delete assumption0/it.unibo.pmsManager/it.unibo.pmsClientPhase directory

--- assumption0/it.unibo.pmsManager/it.unibo.pmsClientPhase/clientphase.pl
+++
@@ -1,9 +0,0 @@
-%====================================================================================
-% clientphase description   
-%====================================================================================
-context(ctxcp, "localhost",  "TCP", "8050").
- qactor( weightsensor, ctxcp, "it.unibo.weightsensor.Weightsensor").
-  qactor( outsonar, ctxcp, "it.unibo.outsonar.Outsonar").
-  qactor( parkmanagerservice, ctxcp, "it.unibo.parkmanagerservice.Parkmanagerservice").
-  qactor( trolley, ctxcp, "it.unibo.trolley.Trolley").
-  qactor( client, ctxcp, "it.unibo.client.Client").
