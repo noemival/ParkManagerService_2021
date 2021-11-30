3021b5124f7d64af4897ee8d2661499851d53991
noemival <49717556+noemival@users.noreply.github.com>
2021-11-30 17:33:09 +0100

    ten commit

--- it.unibo.parkManagerService/it.unibo.sprint3/it.unibo.WebApplicationPms/src/main/kotlin/it/unibo/webApplicationPms/connQak/AppConnection.kt
+++ it.unibo.parkManagerService/it.unibo.sprint3/it.unibo.WebApplicationPms/src/main/kotlin/it/unibo/webApplicationPms/connQak/AppConnection.kt
@@ -1,27 +1,39 @@
 package it.unibo.webApplicationPms.connQak
 
+import com.andreapivetta.kolor.Color
 import it.unibo.kactor.MsgUtil
+import it.unibo.kactor.sysUtil
 
 class AppConnection {
 
-    var connToPms: connQakBase
+    lateinit var connToPms: connQakBase
     var answer = ""
 
-    init {
-        connToPms = connQakBase.create(ConnectionType.TCP)
-        connToPms.createConnection()
-    }
+
+
      fun sendRequest(messageId: String, content: String, qakDestination : String): String{
-        val msg = MsgUtil.buildRequest(senderactor, messageId, content, qakDestination)
+
+         connToPms = connQakBase.create(ConnectionType.TCP)
+         connToPms.createConnection()
+
+         val msg = MsgUtil.buildRequest(senderactor, messageId, content, qakDestination)
         answer = connToPms.requestWithRensponse(msg)
 
         return answer
     }
     fun sendEvent(messageId: String, content: String){
+
+        connToPms = connQakBase.create(ConnectionType.TCP)
+        connToPms.createConnection()
+
         val msg = MsgUtil.buildEvent(senderactor, messageId,content)
         connToPms.emit(msg)
+        print(msg)
     }
     fun sendForward(messageId: String, content: String, qakDestination : String){
+
+        connToPms = connQakBase.create(ConnectionType.TCP)
+        connToPms.createConnection()
 
         val msg = MsgUtil.buildDispatch(senderactor, messageId, content, qakDestination)
         connToPms.forward(msg)
