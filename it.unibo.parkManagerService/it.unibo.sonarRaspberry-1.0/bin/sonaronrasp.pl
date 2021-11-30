0c46661d5a97d2e18e36b7636978a9360363b6b8
Antonio Iacobelli <iacobelli97@gmail.com>
2021-11-30 10:54:50 +0100

    nine commit

---
+++ it.unibo.parkManagerService/it.unibo.sonarRaspberry-1.0/bin/sonaronrasp.pl
@@ -0,0 +1,10 @@
+%====================================================================================
+% sonaronrasp description   
+%====================================================================================
+mqttBroker("broker.hivemq.com", "1883", "sonar/data").
+context(ctxsonaronrasp, "localhost",  "TCP", "8068").
+ qactor( sonarsimulator, ctxsonaronrasp, "sonarSimulator").
+  qactor( sonardatasource, ctxsonaronrasp, "sonarHCSR04Support2021").
+  qactor( datacleaner, ctxsonaronrasp, "dataCleaner").
+  qactor( sonar, ctxsonaronrasp, "it.unibo.sonar.Sonar").
+tracing.
